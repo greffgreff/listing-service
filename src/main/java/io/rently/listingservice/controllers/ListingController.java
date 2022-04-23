45e84bf17594278d1dd9009aec95adb4e01bfc07
chandle greff <unconfigured@null.spigotmc.org>
2022-04-23 18:32:50 +0200

    Added bugsnag and mailer service to error reporting

--- src/main/java/io/rently/listingservice/controllers/ListingController.java
+++ src/main/java/io/rently/listingservice/controllers/ListingController.java
@@ -9,33 +9,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1")
 public class ListingController {
-    public static final String PREFIX = "/api/v1";
 
     @Autowired
     public ListingService service;
 
-    @GetMapping(PREFIX + "/{id}")
+    @GetMapping("/{id}")
     public ResponseContent handleGetRequest(@PathVariable String id) {
         Listing listing = service.getListingById(id);
         return new ResponseContent.Builder().setData(listing).build();
     }
 
-    @PostMapping(PREFIX + "/")
+    @PostMapping("/")
     public ResponseContent handlePostRequest(@RequestHeader("Authorization") String header, @RequestBody Listing listing) {
         service.verifyOwnership(header, listing);
         service.addListing(listing);
         return new ResponseContent.Builder().setMessage("Successfully added listing to database.").build();
     }
 
-    @DeleteMapping(PREFIX + "/{id}")
+    @DeleteMapping("/{id}")
     public ResponseContent handleDeleteRequest(@RequestHeader("Authorization") String header, @PathVariable String id) {
         service.verifyOwnership(header, id);
         service.deleteById(id);
         return new ResponseContent.Builder().setMessage("Successfully removed listing from database.").build();
     }
 
-    @PutMapping(PREFIX + "/{id}")
+    @PutMapping("/{id}")
     public ResponseContent handlePutRequest(@RequestHeader("Authorization") String header, @PathVariable String id, @RequestBody Listing listing) {
         service.verifyOwnership(header, listing);
         service.putById(id, listing);
--- src/main/java/io/rently/listingservice/exceptions/ExceptionController.java
+++ src/main/java/io/rently/listingservice/exceptions/ExceptionController.java
@@ -1,7 +1,10 @@
 package io.rently.listingservice.exceptions;
 
+import com.bugsnag.Bugsnag;
 import io.rently.listingservice.models.ResponseContent;
+import io.rently.listingservice.services.MailerService;
 import io.rently.listingservice.utils.Broadcaster;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,34 +18,31 @@ import javax.servlet.http.HttpServletResponse;
 @ControllerAdvice
 public class ExceptionController {
 
-    @ExceptionHandler(Exception.class)
+    @Autowired
+    private Bugsnag bugsnag;
+
     @ResponseBody
-    public ResponseContent handleGenericException(HttpServletResponse response, Exception exception) {
+    @ExceptionHandler(Exception.class)
+    public ResponseContent unhandledExceptions(HttpServletResponse response, Exception exception) {
         Broadcaster.error(exception);
         ResponseStatusException resEx = Errors.INTERNAL_SERVER_ERROR;
         response.setStatus(resEx.getStatus().value());
+        MailerService.dispatchErrorToDevs(exception);
+        bugsnag.notify(exception);
         return new ResponseContent.Builder(resEx.getStatus()).setMessage(resEx.getReason()).build();
     }
 
-    @ExceptionHandler(ResponseStatusException.class)
     @ResponseBody
+    @ExceptionHandler(ResponseStatusException.class)
     public static ResponseContent handleResponseException(HttpServletResponse response, ResponseStatusException ex) {
         Broadcaster.httpError(ex);
         response.setStatus(ex.getStatus().value());
         return new ResponseContent.Builder(ex.getStatus()).setMessage(ex.getReason()).build();
     }
 
-    @ExceptionHandler(MethodNotAllowedException.class)
     @ResponseBody
+    @ExceptionHandler({ MethodNotAllowedException.class, HttpRequestMethodNotSupportedException.class })
     public static ResponseContent handleInvalidURI(HttpServletResponse response) {
-        ResponseStatusException resEx = Errors.INVALID_URI_PATH;
-        response.setStatus(resEx.getStatus().value());
-        return new ResponseContent.Builder(resEx.getStatus()).setMessage(resEx.getReason()).build();
-    }
-
-    @ResponseBody
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public static ResponseContent handleInvalidMethod(HttpServletResponse response) {
         ResponseStatusException resEx = Errors.INVALID_URI_PATH;
         response.setStatus(resEx.getStatus().value());
         return new ResponseContent.Builder(resEx.getStatus()).setMessage(resEx.getReason()).build();
